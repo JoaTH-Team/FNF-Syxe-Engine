5cd8e5e9d978e66c71977693ca77a58a0eb4601c
khuonghoanghuy <khuonghoanghuy@gmail.com>
2025-04-15 11:24:37 +0700

    revert code

--- assets/data/stages/stage.lua
+++ assets/data/stages/stage.lua
@@ -3,17 +3,17 @@ function create()
     setProperty("curStage", "stage")
 
     createSprite("bg", {-600, -200}, "stageback")
-    setScrollFactor("bg", 0.9)
-    setScrollFactor("bg", 0.9)
+    setProperty("bg.scrollFactor.x", 0.9)
+    setProperty("bg.scrollFactor.y", 0.9)
     add("bg")
 
     createSprite("stageFront", {-650, 600}, "stagefront")
-    setScrollFactor("stageFront", 0.9)
-    setScrollFactor("stageFront", 0.9)
+    setProperty("stageFront.scrollFactor.x", 0.9)
+    setProperty("stageFront.scrollFactor.y", 0.9)
     add("stageFront")
 
     createSprite("stageCurtains", {-500, -300}, "stagecurtains")
-    setScrollFactor("stageCurtains", 1.3)
-    setScrollFactor("stageCurtains", 1.3)
+    setProperty("stageCurtains.scrollFactor.x", 1.3)
+    setProperty("stageCurtains.scrollFactor.y", 1.3)
     add("stageCurtains")
 end
